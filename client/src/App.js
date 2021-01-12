e917db4e9b433f515c6fe2d00c2d6bbdfa96c456
Gardian <gardianii@hotmail.com>
2021-01-12 21:57:12 +0100

    Home and Register components

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,18 @@ import React from 'react'
 import './App.css';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Nav from './components/Nav'
-
+import Home from './components/Home'
+import Form from './components/Form'
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <Nav />
-     
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        {<Route exact path="/register" component={Form}/>}
+      </Switch>
     </div>
     </BrowserRouter>
   );
---
+++ client/src/components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h1 className="title">All Users</h1>
+            <ul className="users">
+                <li>
+                    Name:John / Email:john@email.com
+                </li>
+                <li>
+                    Name:John / Email:john@email.com
+                </li>
+                <li>
+                    Name:John / Email:john@email.com
+                </li>
+            </ul>
+            
+        </div>
+    )
+}
+
+export default Home
